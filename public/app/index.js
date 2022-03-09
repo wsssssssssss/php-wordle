24c79d46ec38558207c75c17f10d9d3ea3e6f84d
kimjuneseo <june3929213512@gmail.com>
2022-03-09 23:23:02 +0900

    1일차

---
+++ public/app/index.js
@@ -0,0 +1,51 @@
+const $ = (el, option = true) => option ? document.querySelector(el) : document.querySelectorAll(el);
+const writingItemText = (txt) => $(".gameBoard .item", false)[count].innerText = txt;
+const fiveTextComfirm = () => count % 5 ? alert("5글자를 입력해주세요") : false ;
+
+const keyArr = []; 
+let count = -1;
+const onWindowKeyDown = ({key}) => {
+    if(key === 'Backspace' && count !== -1 && count < 30){
+        writingItemText('');
+        count--;
+        return;
+    }
+    
+    if(count < 29){
+        count++;
+        console.log(count)
+        if(key === 'Enter'){
+            fiveTextComfirm();
+            count--;
+            return
+        }
+        if(key.match(/[^0-9`~!@#$%^&*()_|+\-=?;:'",.<>\{\}\[\]\\\/]/) && key.length === 1  &&  key !== ' '){
+            writingItemText(key.toUpperCase());
+            return;
+        }
+        count--;
+    }    
+};
+
+const onItemClick = ({target}) => {
+    if(count < 30){
+        count++;
+        if(target.innerText === 'ENTER'){
+            fiveTextComfirm();
+            return;
+        }
+        
+        if(target.innerText === '' && count > 0){
+            count--;
+            writingItemText('');
+            count--;
+            return;
+        }
+        writingItemText(target.innerText);
+    }
+};
+
+window.addEventListener("keydown", onWindowKeyDown);
+$(".item", false).forEach(e => e.addEventListener("click", onItemClick));
+
+
